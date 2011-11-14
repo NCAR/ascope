854ae48d4250087eb9b86425d891396672832e91
Charles Martin <martinc@ucar.edu>
2011-11-14 00:12:27 +0000

    Get rid of Doxyfiles.
    git-svn-id: http://svn.eol.ucar.edu/svn/eol/common/trunk/ascope@8127 da89c675-a4d7-0310-8a15-9e7546503515

--- tool_ascope.py
+++ tool_ascope.py
@@ -26,7 +26,7 @@ AScope.h
 PlotInfo.h
 """)
 
-html = env.Apidocs(sources + headers, DOXYFILE_FILE = "Doxyfile")
+html = env.Apidocs(sources + headers, DOXYFILE_DICT={'PROJECT_NAME':'ascope', 'PROJECT_NUMBER':'1.0'})
 
 ascope = env.Library('ascope', sources)
 
